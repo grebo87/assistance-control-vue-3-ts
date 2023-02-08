748cd3eafd8195d789ce1e51c66eb8e47bc05a93
grebo <grebodeveloper@gmail.com>
2023-02-08 17:47:44 -0400

    update

--- src/views/assistance/Index.vue
+++ src/views/assistance/Index.vue
@@ -14,7 +14,7 @@ const { message, statusRespose} = storeToRefs(personalStore);
         <div class="container">
             <section class="content-header">
                 <div class="container-fluid">
-                    <div class="row mb-2">
+                    <!-- <div class="row mb-2">
 
                         <div class="col">
                             <router-link :to="{ name: 'personal.create' }" class="btn btn-primary float-right">
@@ -22,7 +22,7 @@ const { message, statusRespose} = storeToRefs(personalStore);
                                 Registrar Inasistencia
                             </router-link>
                         </div>
-                    </div>
+                    </div> -->
                     <div v-if="message" :class="statusRespose == true ? 'alert alert-success' : 'alert alert-danger'">
                         {{ message }}
                     </div>
